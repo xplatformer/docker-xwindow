ac038dea4af6508bd7163883daf65334ddcac57a
jrbeverly <jrbeverly@live.com>
2017-09-04 15:12:53 -0400

    Restructuring the build scripts

---
+++ build/ci/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+#
+# Variables
+#
+DIR="$(dirname $(readlink -f "$0"))"
+DIR_ROOT="$(dirname $DIR)"
+DIR_BUILD="${DIR_ROOT}/build"
+DIR_VERSIONS="${DIR_ROOT}/versions"
+
+#
+# Executing
+#
+
+echo "# Pulling images for all version tags"
+for dir in $DIR_VERSIONS/*/; 
+do 
+  version=$(basename ${dir%*/}); 
+
+  make -s -C "${DIR_BUILD}" VERSION=${version} pull
+done
+
+echo "# Preparing image tags for release"
+for dir in $DIR_VERSIONS/*/; 
+do 
+  version=$(basename ${dir%*/}); 
+  
+  make -s -C "${DIR_BUILD}" VERSION=${version} release
+done
+
+echo "# Deploying the image tags"
+for dir in $DIR_VERSIONS/*/; 
+do 
+  version=$(basename ${dir%*/}); 
+  
+  make -s -C "${DIR_BUILD}" VERSION=${version} deploy
+done
---
+++ build/ci/test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#
+# Variables
+#
+DIR="$(dirname $(readlink -f "$0"))"
+DIR_ROOT="$(dirname $DIR)"
+DIR_BUILD="${DIR_ROOT}/build"
+DIR_VERSIONS="${DIR_ROOT}/versions"
+
+#
+# Executing
+#
+for dir in $DIR_VERSIONS/*/; 
+do 
+  version=$(basename ${dir%*/}); 
+  
+  make -s -C "${DIR_BUILD}" VERSION=${version} test
+done
